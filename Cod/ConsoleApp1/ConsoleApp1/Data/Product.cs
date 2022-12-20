51415abddff9d2dd23551c9e7a86abd4a114bab3
Cristian HENEGARIU <74611433+Henegarica@users.noreply.github.com>
2022-12-20 18:52:19 +0200

    Added the main code

---
+++ Cod/ConsoleApp1/ConsoleApp1/Data/Product.cs
@@ -0,0 +1,29 @@
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace ConsoleApp1.Data;
+
+public record Quantity
+{
+    public uint Value { get; }
+
+    private Quantity(uint value)
+    {
+        Value = value;
+    }
+
+    public Option<Quantity> Create(int value)
+        => (value > 0) ? Some(new Quantity((uint)value)) : Option<Quantity>.None;
+}
+
+public record Product(int Id, Quantity Quantity, uint Price, uint Stock);
+
+public record Order
+{
+    private Order(int id, Product[] products)
+    {
+    }
+
+    public static Option<Order> Create(int id, Product[] products) =>
+        Some(new Order(id, products));
+}
---
+++ Cod/ConsoleApp1/ConsoleApp1/Workflows/CreateCommandWorkflow.cs
@@ -0,0 +1,58 @@
+using ConsoleApp1.Data;
+using CSharp.Choices;
+using static LanguageExt.Prelude;
+using LanguageExt;
+using Array = System.Array;
+
+namespace ConsoleApp1;
+
+public record CreateCommand();
+
+[AsChoice]
+public static partial class CreateCommandResult
+{
+    public interface ICreateCommandResult
+    {
+    }
+
+    public record CommandCreated(Order Order) : ICreateCommandResult;
+
+    public record CreateCommandFailed(string Reason) : ICreateCommandResult;
+}
+
+public class CreateCommandWorkflow
+{
+    public CreateCommandWorkflow()
+    {
+        //read data from file
+    }
+
+    public async Task<CreateCommandResult.ICreateCommandResult> ExecuteAsync(CreateCommand cmd)
+    {
+        Either<string, Order> expr = from products in LoadProducts()
+            from isValid in ValidateCommand(cmd)
+            from order in Order.Create(123, products).ToEither("Cannot create a valid Order")
+            from saveOrder in SaveOrder(order)
+            select order;
+
+        return expr.Match(
+            Right: order => (CreateCommandResult.ICreateCommandResult)new CreateCommandResult.CommandCreated(order),
+            Left: error => new CreateCommandResult.CreateCommandFailed(error));
+    }
+
+    public Either<string, Product[]> LoadProducts()
+    {
+        return Right<string, Product[]>(Array.Empty<Product>());
+    }
+
+    public Either<string, Unit> ValidateCommand(CreateCommand cmd)
+    {
+        //presupunem ca avem o comanda valida
+        return Right<string, Unit>(unit);
+    }
+
+    public Either<string, Unit> SaveOrder(Order order)
+    {
+        return Right<string, Unit>(unit);
+    }
+}
